9f6e6c2423c37a7c7755317ed489c681d5ae114e
Joachim Metz <joachim.metz@gmail.com>
2016-03-13 15:47:44 +0100

    Changes for migration to pylint 1.5

--- dfwinreg/__init__.py
+++ dfwinreg/__init__.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
+"""Digital Forensics Windows Registry (dfWinReg).
+
+dfWinReg, or Digital Forensics Windows Registry, is a Python module
+that provides read-only access to Windows Registry objects.
+"""
 
 __version__ = '20160227'
