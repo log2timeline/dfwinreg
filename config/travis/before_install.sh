29e3bcbd0f9a64882f0cd02225a221a2e7a5445e
Joachim Metz <joachim.metz@gmail.com>
2016-07-30 12:19:48 +0200

    Initial Travis Mac OS X support

--- config/travis/before_install.sh
+++ config/travis/before_install.sh
@@ -11,7 +11,16 @@ PYTHON3_DEPENDENCIES="libregf-python3 python3-construct python3-dfdatetime pytho
 # Exit on error.
 set -e;
 
-if test `uname -s` = "Linux";
+if test `uname -s` = "Darwin";
+then
+	git clone https://github.com/log2timeline/l2tdevtools.git;
+
+	mv l2tdevtools ../;
+	mkdir dependencies;
+
+	PYTHONPATH=../l2tdevtools ../l2tdevtools/tools/update.py --download-directory=dependencies --preset=dfwinreg;
+
+elif test `uname -s` = "Linux";
 then
 	sudo add-apt-repository ppa:gift/dev -y;
 	sudo apt-get update -q;
