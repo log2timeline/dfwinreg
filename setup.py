fd835d020a479c1b085d0565d54df6732f476a68
Joachim Metz <joachim.metz@gmail.com>
2017-05-26 06:20:16 +0200

    Code review: 324900043: Changed setup.py and dpgk files

--- setup.py
+++ setup.py
@@ -3,18 +3,22 @@
 """Installation and deployment script."""
 
 from __future__ import print_function
-import os
 import sys
 
 try:
-  from setuptools import find_packages, setup, Command
+  from setuptools import find_packages, setup
 except ImportError:
-  from distutils.core import find_packages, setup, Command
+  from distutils.core import find_packages, setup
 
 try:
-  from setuptools.commands.bdist_rpm import bdist_rpm
+  from distutils.command.bdist_msi import bdist_msi
 except ImportError:
+  bdist_msi = None
+
+try:
   from distutils.command.bdist_rpm import bdist_rpm
+except ImportError:
+  bdist_rpm = None
 
 if sys.version < '2.7':
   print('Unsupported Python version: {0:s}.'.format(sys.version))
@@ -24,81 +28,92 @@ if sys.version < '2.7':
 # Change PYTHONPATH to include dfwinreg so that we can get the version.
 sys.path.insert(0, '.')
 
-import dfwinreg
+import dfwinreg  # pylint: disable=wrong-import-position
 
 
-class BdistRPMCommand(bdist_rpm):
-  """Custom handler for the bdist_rpm command."""
+if not bdist_msi:
+  BdistMSICommand = None
+else:
+  class BdistMSICommand(bdist_msi):
+    """Custom handler for the bdist_msi command."""
 
-  def _make_spec_file(self):
-    """Generates the text of an RPM spec file.
+    def run(self):
+      """Builds an MSI."""
+      # Command bdist_msi does not support the library version, neither a date
+      # as a version but if we suffix it with .1 everything is fine.
+      self.distribution.metadata.version += '.1'
 
-    Returns:
-      A list of strings containing the lines of text.
-    """
-    # Note that bdist_rpm can be an old style class.
-    if issubclass(BdistRPMCommand, object):
-      spec_file = super(BdistRPMCommand, self)._make_spec_file()
-    else:
-      spec_file = bdist_rpm._make_spec_file(self)
-
-    if sys.version_info[0] < 3:
-      python_package = 'python'
-    else:
-      python_package = 'python3'
-
-    description = []
-    summary = ''
-    in_description = False
-
-    python_spec_file = []
-    for index, line in enumerate(spec_file):
-      if line.startswith('Summary: '):
-        summary = line
-
-      elif line.startswith('BuildRequires: '):
-        line = 'BuildRequires: {0:s}-setuptools'.format(python_package)
-
-      elif line.startswith('Requires: '):
-        if python_package == 'python3':
-          line = line.replace('python', 'python3')
-
-      elif line.startswith('%description'):
-        in_description = True
-
-      elif line.startswith('%files'):
-        line = '%files -f INSTALLED_FILES -n {0:s}-%{{name}}'.format(
-           python_package)
-
-      elif line.startswith('%prep'):
-        in_description = False
-
-        python_spec_file.append(
-            '%package -n {0:s}-%{{name}}'.format(python_package))
-        python_spec_file.append('{0:s}'.format(summary))
-        python_spec_file.append('')
-        python_spec_file.append(
-            '%description -n {0:s}-%{{name}}'.format(python_package))
-        python_spec_file.extend(description)
-
-      elif in_description:
-        # Ignore leading white lines in the description.
-        if not description and not line:
-          continue
-
-        description.append(line)
-
-      python_spec_file.append(line)
-
-    return python_spec_file
+      bdist_msi.run(self)
 
 
-dfwinreg_version = dfwinreg.__version__
+if not bdist_rpm:
+  BdistRPMCommand = None
+else:
+  class BdistRPMCommand(bdist_rpm):
+    """Custom handler for the bdist_rpm command."""
 
-# Command bdist_msi does not support the library version, neither a date
-# as a version but if we suffix it with .1 everything is fine.
-if 'bdist_msi' in sys.argv:
-  dfwinreg_version += '.1'
+    def _make_spec_file(self):
+      """Generates the text of an RPM spec file.
+
+      Returns:
+        list[str]: lines of the RPM spec file.
+      """
+      # Note that bdist_rpm can be an old style class.
+      if issubclass(BdistRPMCommand, object):
+        spec_file = super(BdistRPMCommand, self)._make_spec_file()
+      else:
+        spec_file = bdist_rpm._make_spec_file(self)
+
+      if sys.version_info[0] < 3:
+        python_package = 'python'
+      else:
+        python_package = 'python3'
+
+      description = []
+      summary = ''
+      in_description = False
+
+      python_spec_file = []
+      for line in iter(spec_file):
+        if line.startswith('Summary: '):
+          summary = line
+
+        elif line.startswith('BuildRequires: '):
+          line = 'BuildRequires: {0:s}-setuptools'.format(python_package)
+
+        elif line.startswith('Requires: '):
+          if python_package == 'python3':
+            line = line.replace('python', 'python3')
+
+        elif line.startswith('%description'):
+          in_description = True
+
+        elif line.startswith('%files'):
+          line = '%files -f INSTALLED_FILES -n {0:s}-%{{name}}'.format(
+              python_package)
+
+        elif line.startswith('%prep'):
+          in_description = False
+
+          python_spec_file.append(
+              '%package -n {0:s}-%{{name}}'.format(python_package))
+          python_spec_file.append('{0:s}'.format(summary))
+          python_spec_file.append('')
+          python_spec_file.append(
+              '%description -n {0:s}-%{{name}}'.format(python_package))
+          python_spec_file.extend(description)
+
+        elif in_description:
+          # Ignore leading white lines in the description.
+          if not description and not line:
+            continue
+
+          description.append(line)
+
+        python_spec_file.append(line)
+
+      return python_spec_file
+
 
 dfwinreg_description = (
     'Digital Forensics Windows Registry (dfWinReg).')
@@ -109,7 +124,7 @@ dfwinreg_long_description = (
 
 setup(
     name='dfwinreg',
-    version=dfwinreg_version,
+    version=dfwinreg.__version__,
     description=dfwinreg_description,
     long_description=dfwinreg_long_description,
     license='Apache License, Version 2.0',
@@ -117,6 +132,7 @@ setup(
     maintainer='dfWinReg development team',
     maintainer_email='log2timeline-dev@googlegroups.com',
     cmdclass={
+        'bdist_msi': BdistMSICommand,
         'bdist_rpm': BdistRPMCommand},
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -131,6 +147,6 @@ setup(
     },
     data_files=[
         ('share/doc/dfwinreg', [
-            'AUTHORS', 'ACKNOWLEDGEMENTS', 'LICENSE', 'README']),
+            'ACKNOWLEDGEMENTS', 'AUTHORS', 'LICENSE', 'README']),
     ],
 )
