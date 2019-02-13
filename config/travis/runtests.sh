a25c0f6e7f5f8623701dd501fe2095af0e30f45d
Joachim Metz <joachim.metz@gmail.com>
2019-02-13 08:41:09 +0100

    Applied updates and added Fedora Core CI tests (#111)

--- config/travis/runtests.sh
+++ config/travis/runtests.sh
@@ -38,6 +38,19 @@ then
 		PYTHONPATH=. python ./tests/end-to-end.py --debug -c config/end-to-end.ini;
 	fi
 
+elif test -n "${FEDORA_VERSION}";
+then
+	CONTAINER_NAME="fedora${FEDORA_VERSION}";
+
+	docker exec "${CONTAINER_NAME}" sh -c "git clone https://github.com/log2timeline/dfwinreg.git";
+
+	if test ${TRAVIS_PYTHON_VERSION} = "2.7";
+	then
+		docker exec "${CONTAINER_NAME}" sh -c "cd dfwinreg && python2 run_tests.py";
+	else
+		docker exec "${CONTAINER_NAME}" sh -c "cd dfwinreg && python3 run_tests.py";
+	fi
+
 elif test "${TRAVIS_OS_NAME}" = "linux";
 then
 	COVERAGE="/usr/bin/coverage";
