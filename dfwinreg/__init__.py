ac5d7f139b2427f76e337b2187f60659f4e5de8d
Joachim Metz <joachim.metz@gmail.com>
2020-04-15 06:35:32 +0200

    Updated version

--- dfwinreg/__init__.py
+++ dfwinreg/__init__.py
@@ -5,4 +5,4 @@ dfWinReg, or Digital Forensics Windows Registry, is a Python module
 that provides read-only access to Windows Registry objects.
 """
 
-__version__ = '20200211'
+__version__ = '20200415'
