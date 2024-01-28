b01c75af0fd03abb5745d2b7e16511eb08354912
Joachim Metz <joachim.metz@gmail.com>
2024-01-28 13:12:46 +0100

    Changes to use docformatter

--- dfwinreg/__init__.py
+++ dfwinreg/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """Digital Forensics Windows Registry (dfWinReg).
 
-dfWinReg, or Digital Forensics Windows Registry, is a Python module
-that provides read-only access to Windows Registry objects.
+dfWinReg, or Digital Forensics Windows Registry, is a Python module that
+provides read-only access to Windows Registry objects.
 """
 
-__version__ = '20240113'
+__version__ = '20240128'
